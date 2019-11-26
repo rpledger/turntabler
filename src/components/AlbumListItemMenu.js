173761dce2ee6c429e7c0b8723cd9dba9996a174
Rebecca Pledger <rebeccapledger14@gmail.com>
2019-11-25 21:12:53 -0600

    Breakout popup

--- src/components/AlbumListItemMenu.js
+++ src/components/AlbumListItemMenu.js
@@ -26,7 +26,8 @@ import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
 import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 import InfoIcon from '@material-ui/icons/Info';
 
-import AlbumListItemMenu from './AlbumListItemMenu';
+
+
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -41,34 +42,6 @@ const useStyles = makeStyles(theme => ({
   },
 
 }));
-
-var tileData = [
-  {
-    img: "https://img.discogs.com/SwnFq01J1XAXArAhfvgtG6EgkH0=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-986527-1293716829.jpeg.jpg",
-    title: "Emotionalism",
-    artist: "Avett Brothers"
-  },
-  {
-    img: "https://img.discogs.com/7XGz7VuFH-dp80PqS_M-BLe7GGA=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1963341-1262735484.jpeg.jpg",
-    title: "I and Love and You",
-    artist: "Avett Brothers"
-  },
-  {
-    img: "https://img.discogs.com/7thNTBY7jzWL6Oa7QXwCssboU7k=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-2093811-1263645509.jpeg.jpg",
-    title: "The Second Gleam",
-    artist: "Avett Brothers"
-  },
-  {
-    img: "https://img.discogs.com/7thNTBY7jzWL6Oa7QXwCssboU7k=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-2093811-1263645509.jpeg.jpg",
-    title: "The Second Gleam",
-    artist: "Avett Brothers"
-  },
-  {
-    img: "https://img.discogs.com/7thNTBY7jzWL6Oa7QXwCssboU7k=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-2093811-1263645509.jpeg.jpg",
-    title: "The Second Gleam",
-    artist: "Avett Brothers"
-  }
-]
 
 export default function AlbumListItem(props) {
   const classes = useStyles();
@@ -99,15 +72,49 @@ export default function AlbumListItem(props) {
 
   return (
     <div className={classes.root}>
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar variant="square" className={classes.bigAvatar} alt="Remy Sharp" src={props.img} />
-            </ListItemAvatar>
-            <ListItemText primary={props.title} secondary={props.artist}/>
-            <ListItemIcon>
-              <AlbumListItemMenu title={props.title} />
-            </ListItemIcon>
-          </ListItem>
+      <IconButton
+        ref={anchorRef}
+        aria-controls={open ? 'menu-list-grow' : undefined}
+        aria-haspopup="true"
+        onClick={handleToggle}
+      >
+        <MoreVertIcon/>
+      </IconButton>
+      <Menu
+        id="menu-appbar"
+        anchorEl={anchorRef.current}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        open={open}
+        onClose={handleClose}
+      >
+        <ListSubheader onClick={handleClose}>{props.title}</ListSubheader>
+        <Divider component="li" />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <PlayCircleOutlineIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Listen Now" />
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <CalendarTodayIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Add Past Listen" />
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <InfoIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="More Info" />
+        </MenuItem>
+      </Menu>
     </div>
   );
 }
