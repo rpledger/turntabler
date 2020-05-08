bc0c964e8815065679a97e2be636d7d6f7985694
Rebecca Pledger <rebeccapledger14@gmail.com>
2020-05-07 19:43:08 -0500

    Clean up files

--- api/add_user_release_listen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-user_id=$1
-release_id=$2
-
-curl -XPOST -H "Content-type: application/json" -d \
-'{"release_id": "'"$release_id"'", "user_id": "'"$user_id"'"}' \
-'127.0.0.1:4000/listens'
