ccea64b6ce7b646e6e6d8e88b204555a1ea1bd2e
Rebecca Pledger <rebeccapledger14@gmail.com>
2020-05-07 21:43:35 -0500

    Adding login and signup functions

--- api/turn_data/app.py
+++ api/turn_data/app.py
@@ -28,7 +28,7 @@ def create_app():
 
 app = create_app()
 
-user = User(id=0, username="test", email="test@test.com")
+user = User(id=0, username="test")
 user.set_password("password")
 
 release1 = Release(artist="Avett Brothers", title="Emotionalism", thumb="https://img.discogs.com/SwnFq01J1XAXArAhfvgtG6EgkH0=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-986527-1293716829.jpeg.jpg")
@@ -48,23 +48,40 @@ db.session.commit()
 @app.route('/users', methods=['GET'])
 def get_users():
     users = User.query.all()
-    user_dict = dict()
+    result = []
     for user in users:
-        user_dict[user.id] = user.to_json()
-    return user_dict
+        result.append(user.to_json())
+
+    return result
 
 
-@app.route('/users', methods=['POST'])
-def add_user():
-    data = request.get_json()
-    id = data['id']
-    username = data['username']
-    email = data['email']
+@app.route('/signup', methods=['GET', 'POST'])
+def signup_user():
+ data = request.get_json()
 
-    user = User(id=id, username=username, email=email)
-    user.set_password("password")
-    db.session.add(user)
-    db.session.commit()
+ new_user = User(name=data['name'])
+ user.set_password("password")
+ db.session.add(new_user)
+ db.session.commit()
+
+ return jsonify({'message': 'registered successfully'})
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_user():
+
+  auth = request.authorization
+
+  if not auth or not auth.username or not auth.password:
+     return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})
+
+  user = Users.query.filter_by(name=auth.username).first()
+
+  if check_password(auth.password):
+     token = jwt.encode({'public_id': user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+     return jsonify({'token' : token.decode('UTF-8')})
+
+  return make_response('could not verify',  401, {'WWW.Authentication': 'Basic realm: "login required"'})
 
 
 @app.route('/releases', methods=['GET'])
