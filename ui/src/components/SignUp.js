22b6da155582b3b528c3009ab06c6e827300eb2b
Rebecca Pledger <rebeccapledger14@gmail.com>
2020-07-11 19:33:47 -0500

    Simplify Signup

--- ui/src/components/SignUp.js
+++ ui/src/components/SignUp.js
@@ -118,29 +118,6 @@ class SignUp extends React.Component {
         </Typography>
         <form className={classes.form} noValidate action="/albums" onSubmit={this.handleSubmit}>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-              />
-            </Grid>
             <Grid item xs={12}>
               <TextField
                 variant="outlined"
